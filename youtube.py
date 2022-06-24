3a83953e4660261ea2b03c6eee144f8c2c907ea0
Coder73 <87517710+codder73@users.noreply.github.com>
2022-06-24 11:40:29 +0530

    Final Commit

---
+++ youtube.py
@@ -0,0 +1,25 @@
+from tkinter import *
+from pytube import YouTube
+
+root = Tk()
+root.geometry('1000x500')
+root.resizable(0,0)
+root.title("youtube video downloader")
+Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
+
+##enter link
+link = StringVar()
+
+Label(root, text = 'Paste Link Here:', font = 'arial 15 bold').place(x= 160 , y = 80)
+link_enter = Entry(root, width = 70,textvariable = link).place(x = 32, y = 160)
+
+#function to download video
+def Downloader():
+    url =YouTube(str(link.get()))
+    video = url.streams.get_highest_resolution()
+    video.download()
+    Label(root, text = 'DOWNLOADED', font = 'arial 15').place(x= 180 , y = 220)
+
+Button(root,text = 'DOWNLOAD', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2, command = Downloader).place(x=180 ,y = 220)
+
+root.mainloop()
